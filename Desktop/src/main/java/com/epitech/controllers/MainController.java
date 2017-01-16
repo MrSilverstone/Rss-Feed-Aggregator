6365898d8c64b773032ae0cf5edd65d6268518ca
Louis Mondesir <louis.mondesir@gmail.com>
2017-01-16 22:57:23 +0100

    Desktop : add navigation drawer

--- Desktop/src/main/java/com/epitech/controllers/MainController.java
+++ Desktop/src/main/java/com/epitech/controllers/MainController.java
@@ -7,10 +7,13 @@ import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
 import com.jfoenix.transitions.hamburger.HamburgerNextArrowBasicTransition;
 import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -29,13 +32,21 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        HamburgerBackArrowBasicTransition burgerTask = new HamburgerBackArrowBasicTransition(hamburger);
 
-        burgerTask.setRate(-1);
+        try {
+            VBox box = FXMLLoader.load(getClass().getResource("/views/NavigationDrawer.fxml"));
+            drawer.setSidePane(box);
+        } catch (IOException ex) {
+
+        }
+
+        HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamburger);
+
+        transition.setRate(-1);
 
         hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e)->{
-            burgerTask.setRate(burgerTask.getRate()*-1);
-            burgerTask.play();
+            transition.setRate(transition.getRate()*-1);
+            transition.play();
 
             if (drawer.isShown()){
                 drawer.close();
---
+++ Desktop/src/main/java/com/epitech/controllers/NavigationDrawerController.java
@@ -0,0 +1,16 @@
+package com.epitech.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by loulo on 16/01/2017.
+ */
+public class NavigationDrawerController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
